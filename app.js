e17c094566659b1028dfb2f8abf113499f755a40
DAN NELSON <dannelson@DANs-Air.lan>
2021-01-16 12:21:49 -0700

    start using inquirer package

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-    console.log('Portfolio complete Check out index.html to see the output');
-  });
+//     console.log('File has been created successfully')
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
